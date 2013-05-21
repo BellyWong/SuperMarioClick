fd85d422d43e3e0a6fe3447a1e2adaa4be59ddfc
liu weimu <lwm@lius.(none)>
2013-05-21 13:23:58 +0900

    Initial Commit

---
+++ SuperMarioClick/AppDelegate.h
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.h
+//  SuperMarioClick
+//
+//  Created by liu weimu on 5/21/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "cocos2d.h"
+
+// Added only for iOS 6 support
+@interface MyNavigationController : UINavigationController <CCDirectorDelegate>
+@end
+
+@interface AppController : NSObject <UIApplicationDelegate>
+{
+	UIWindow *window_;
+	MyNavigationController *navController_;
+	
+	CCDirectorIOS	*director_;							// weak ref
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (readonly) MyNavigationController *navController;
+@property (readonly) CCDirectorIOS *director;
+
+@end
---
+++ SuperMarioClick/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SuperMarioClick
+//
+//  Created by liu weimu on 5/21/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
